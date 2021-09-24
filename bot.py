a350b21e114924d3ca887b5f5733fba6adbadeb1
alex-tjellesen <alex.tjellesen@gmail.com>
2021-09-24 18:21:14 -0500

    added action through harvest

--- bot.py
+++ bot.py
@@ -17,13 +17,18 @@ from model.player import Player
 from api.constants import Constants
 
 import random
+import copy
 
 logger = Logger()
 constants = Constants()
 
 boughtAll = False
-plantxPos = list[12, 13, 14, 15, 16, 17]
-harvestxPos = plantPos.copy()
+plantAll = False
+plantxPos = [12, 13, 14, 15, 16, 17]
+harvestxPos = copy.deepcopy(plantxPos)
+cropsToBuy = [1,2,3,4,6,7]
+cropsToPlant = copy.deepcopy(cropsToBuy)
+readyToPlant = False
 
 def get_move_decision(game: Game) -> MoveDecision:
     """
@@ -82,6 +87,7 @@ def get_move_decision(game: Game) -> MoveDecision:
     elif len(my_player.seed_inventory) > 0:
         logger.debug("Moving towards specific planting location")
         decision = MoveDecision(move_towards(Position(plantxPos.pop(), 40)))
+        readyToPlant = True
 
 
 # -------------------------------------------------------------
@@ -108,39 +114,76 @@ def get_action_decision(game: Game) -> ActionDecision:
     # Select your decision here!
     my_player: Player = game_state.get_my_player()
     pos: Position = my_player.position
-    # Let the crop of focus be the one we have a seed for, if not just choose a random crop
-    crop = max(my_player.seed_inventory, key=my_player.seed_inventory.get) \
-        if sum(my_player.seed_inventory.values()) > 0 else random.choice(list(CropType))
+ # -------------------------------------------------------------
+ #    # Let the crop of focus be the one we have a seed for, if not just choose a random crop
+ #    crop = max(my_player.seed_inventory, key=my_player.seed_inventory.get) \
+ #        if sum(my_player.seed_inventory.values()) > 0 else random.choice(list(CropType))
+ #
+ #    # Get a list of possible harvest locations for our harvest radius
+ #    possible_harvest_locations = []
+ #    harvest_radius = my_player.harvest_radius
+ #    for harvest_pos in game_util.within_harvest_range(game_state, my_player.name):
+ #        if game_state.tile_map.get_tile(harvest_pos.x, harvest_pos.y).crop.value > 0:
+ #            possible_harvest_locations.append(harvest_pos)
+ #
+ #    logger.debug(f"Possible harvest locations={possible_harvest_locations}")
+ #
+ #    # If we can harvest something, try to harvest it
+ #    if len(possible_harvest_locations) > 0:
+ #        decision = HarvestDecision(possible_harvest_locations)
+ #    # If not but we have that seed, then try to plant it in a fertility band
+ #    elif my_player.seed_inventory[crop] > 0 and \
+ #            game_state.tile_map.get_tile(pos.x, pos.y).type != TileType.GREEN_GROCER and \
+ #            game_state.tile_map.get_tile(pos.x, pos.y).type.value >= TileType.F_BAND_OUTER.value:
+ #        logger.debug(f"Deciding to try to plant at position {pos}")
+ #        decision = PlantDecision([crop], [pos])
+ #    # If we don't have that seed, but we have the money to buy it, then move towards the
+ #    # green grocer to buy it
+ #    elif my_player.money >= crop.get_seed_price() and \
+ #        game_state.tile_map.get_tile(pos.x, pos.y).type == TileType.GREEN_GROCER:
+ #        logger.debug(f"Buy 1 of {crop}")
+ #        decision = BuyDecision([crop], [1])
+ #    # If we can't do any of that, then just do nothing (move around some more)
+ #    else:
+ #        logger.debug(f"Couldn't find anything to do, waiting for move step")
+ #        decision = DoNothingDecision()
+# -------------------------------------------------------------
+    #crop buy focus
+    if len(cropsToBuy) > 0:
+        cropBuy = cropsToBuy.pop()
+    else:
+        cropBuy = 9
+        boughtAll = True
 
-    # Get a list of possible harvest locations for our harvest radius
+    #crop plant focus
+    if len(cropsToPlant) > 0:
+        cropPlant = cropsToPlant.pop()
+    else:
+        cropPlant = 9
+        plantAll = True
+
+     # Get a list of possible harvest locations for our harvest radius
     possible_harvest_locations = []
     harvest_radius = my_player.harvest_radius
     for harvest_pos in game_util.within_harvest_range(game_state, my_player.name):
         if game_state.tile_map.get_tile(harvest_pos.x, harvest_pos.y).crop.value > 0:
             possible_harvest_locations.append(harvest_pos)
-
     logger.debug(f"Possible harvest locations={possible_harvest_locations}")
 
-    # If we can harvest something, try to harvest it
-    if len(possible_harvest_locations) > 0:
+
+    if my_player.position.y == 0 and cropBuy != 9:  #at shop and crops left to buy
+        logger.debug("Buy next crop")
+        decision = BuyDecision([cropBuy], [1])
+    elif readyToPlant == True and plantAll == False: #ready to plant(on planting space) and havent planted all
+        logger.debug("Plant next crop")
+        decision = PlantDecision([cropPlant], my_player.position)
+        readyToPlant = False
+    elif len(possible_harvest_locations) > 0:
         decision = HarvestDecision(possible_harvest_locations)
-    # If not but we have that seed, then try to plant it in a fertility band
-    elif my_player.seed_inventory[crop] > 0 and \
-            game_state.tile_map.get_tile(pos.x, pos.y).type != TileType.GREEN_GROCER and \
-            game_state.tile_map.get_tile(pos.x, pos.y).type.value >= TileType.F_BAND_OUTER.value:
-        logger.debug(f"Deciding to try to plant at position {pos}")
-        decision = PlantDecision([crop], [pos])
-    # If we don't have that seed, but we have the money to buy it, then move towards the
-    # green grocer to buy it
-    elif my_player.money >= crop.get_seed_price() and \
-        game_state.tile_map.get_tile(pos.x, pos.y).type == TileType.GREEN_GROCER:
-        logger.debug(f"Buy 1 of {crop}")
-        decision = BuyDecision([crop], [1])
-    # If we can't do any of that, then just do nothing (move around some more)
     else:
         logger.debug(f"Couldn't find anything to do, waiting for move step")
         decision = DoNothingDecision()
-
+# -------------------------------------------------------------
     logger.debug(f"[Turn {game_state.turn}] Sending ActionDecision: {decision}")
     return decision
 
